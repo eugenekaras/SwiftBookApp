f868c46465f241a65ab456016bcfd8bf00c82dc3
ekaras <key.cpp@gmail.com>
2022-11-24 20:32:42 +0300

    first app

---
+++ SwiftBookApp/SwiftBookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBookAppApp.swift
+//  SwiftBookApp
+//
+//  Created by Евгений Карась on 24.11.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
