9aa31d1973c70bb7682ec507f4bd207a3d2dae93
ekaras <key.cpp@gmail.com>
2022-11-26 19:51:06 +0300

    add date

---
+++ SwiftBookApp/Cell.swift
@@ -0,0 +1,20 @@
+//
+//  Cell.swift
+//  SwiftBookApp
+//
+//  Created by Евгений Карась on 26.11.22.
+//
+
+import SwiftUI
+
+struct Cell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Cell_Previews: PreviewProvider {
+    static var previews: some View {
+        Cell()
+    }
+}
